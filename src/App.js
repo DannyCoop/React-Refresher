d78bab6680d79d9a5b42aa5ae1c228dbc38dec41
Danny <Cooperdaniel5141@gmail.com>
2021-08-16 23:27:46 -0400

    Added a delete tot he task

--- src/App.js
+++ src/App.js
@@ -24,12 +24,17 @@ function App() {
     },
 ])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((tasks) => tasks.id !== id))
+  }
+
   return (
     <div className="container">
-      <Header 
-        title = "React Refresher"
-        />
-        <Tasks tasks={tasks}/>
+      <Header/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
+      ) : ("No Task To Show")}
     </div>
   );
 }
